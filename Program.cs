940c2f252fe9b5e90f97b4c2679235fcad605838
SUmmerInDecember <gabe_fish@protonmail.com>
2024-08-14 03:27:37 +0100

    lol

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Terminal.Gui;
+
+// I absolutely hate the top level statements :(((((((
+
+Application.Run<MainWindow>();
+
+Application.Shutdown();
+class MainWindow : Window
+{
+    public MainWindow()
+    {
+        Title = "VulnHawk - CLI version";
+    }
+}
